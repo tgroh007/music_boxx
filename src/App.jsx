47380bf5696e31aeb44af0dcaa60e9efbd48289d
Thomas Groh <listallmobiles@gmail.com>
2026-01-23 11:12:48 -0800

    Converted to responsive d
    Signed-off-by: Thomas Groh <listallmobiles@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Footer from "./components/Footer"; // ← add footer import
 
 import Intro from "./pages/Intro";
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./Navbar.css"; // Create this new CSS file
+import "./Navbar.css";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => setOpen(!open);
+
   return (
     <header className="navbar">
-      <NavLink to="/" className="nav-link">Intro</NavLink>
-      <NavLink to="/work" className="nav-link">Work</NavLink>
-      <NavLink to="/about" className="nav-link">About</NavLink>
-      <NavLink to="/contact" className="nav-link">Contact</NavLink>
+      <div className="navbar-container">
+        {/* Logo now links to home */}
+        <NavLink to="/" className="logo" onClick={() => setOpen(false)}>
+          The Music Boxx
+        </NavLink>
+
+        {/* Hamburger button for mobile */}
+        <button className="hamburger" onClick={toggleMenu}>
+          ☰
+        </button>
+
+        {/* Links */}
+        <nav className={`nav-links ${open ? "open" : ""}`}>
+          <NavLink to="/" className="nav-link" onClick={() => setOpen(false)}>Intro</NavLink>
+          <NavLink to="/work" className="nav-link" onClick={() => setOpen(false)}>Work</NavLink>
+          <NavLink to="/about" className="nav-link" onClick={() => setOpen(false)}>About</NavLink>
+          <NavLink to="/contact" className="nav-link" onClick={() => setOpen(false)}>Contact</NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
