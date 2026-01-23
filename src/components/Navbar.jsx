07617d15d18aed0d9cf48100c49fe01a839d716b
Thomas Groh <listallmobiles@gmail.com>
2026-01-23 10:54:21 -0800

    smooth slide-in underline on hover
    Signed-off-by: Thomas Groh <listallmobiles@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,13 @@
 import { NavLink } from "react-router-dom";
-
-const linkStyle = ({ isActive }) => ({
-  textDecoration: "none",
-  color: isActive ? "#1976d2" : "#333",
-  fontWeight: "500",
-});
+import "./Navbar.css"; // Create this new CSS file
 
 export default function Navbar() {
   return (
-    <header
-      style={{
-        position: "sticky",
-        top: 0,
-        left: 0,
-        right: 0,
-        height: "60px",
-        display: "flex",
-        alignItems: "center",
-        gap: "30px",
-        padding: "0 40px",
-        borderBottom: "1px solid #ddd",
-        background: "#ffffff",
-        zIndex: 1000,
-      }}
-    >
-      <NavLink to="/" style={linkStyle}>Intro</NavLink>
-      <NavLink to="/work" style={linkStyle}>Work</NavLink>
-      <NavLink to="/about" style={linkStyle}>About</NavLink>
-      <NavLink to="/contact" style={linkStyle}>Contact</NavLink>
+    <header className="navbar">
+      <NavLink to="/" className="nav-link">Intro</NavLink>
+      <NavLink to="/work" className="nav-link">Work</NavLink>
+      <NavLink to="/about" className="nav-link">About</NavLink>
+      <NavLink to="/contact" className="nav-link">Contact</NavLink>
     </header>
   );
 }
