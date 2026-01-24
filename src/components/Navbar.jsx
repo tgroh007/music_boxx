acf3502d7de65c84644cc5197ad07b1ffb0095a6
Thomas Groh <listallmobiles@gmail.com>
2026-01-23 16:12:57 -0800

    added favicon
    Signed-off-by: Thomas Groh <listallmobiles@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,14 @@ import "./Navbar.css";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
-
   const toggleMenu = () => setOpen(!open);
 
   return (
     <header className="navbar">
       <div className="navbar-container">
-        {/* Logo now links to home */}
+        {/* Logo SVG linking to home */}
         <NavLink to="/" className="logo" onClick={() => setOpen(false)}>
-          The Music Boxx
+          <img src="/musicboxx-logo.svg" alt="MusicBoxx Logo" className="logo-svg" />
         </NavLink>
 
         {/* Hamburger button for mobile */}
@@ -20,7 +19,7 @@ export default function Navbar() {
           ☰
         </button>
 
-        {/* Links */}
+        {/* Navbar links */}
         <nav className={`nav-links ${open ? "open" : ""}`}>
           <NavLink to="/" className="nav-link" onClick={() => setOpen(false)}>Intro</NavLink>
           <NavLink to="/work" className="nav-link" onClick={() => setOpen(false)}>Work</NavLink>
